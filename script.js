d098920d804adfe31a1fe7e113167b8c2addc587
SomashMargarita <maargaritasivceva@gmail.come>
2022-08-09 16:31:49 +0300

    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,94 @@
+// const salaries1 = {
+// Manager: { salary: 1000, tax: "10%" },
+// Designer: { salary: 600, tax: "30%" },
+// Artist: { salary: 1500, tax: "15%" },
+// }
+// const team1 = [
+// { name: "Misha", specialization: "Manager" },
+// { name: "Max", specialization: "Designer" },
+// { name: "Vova", specialization: "Designer"},
+// { name: "Leo", specialization: "Artist" },]
+// let valuesKey;
+// let keys = Object.keys(salaries1) //['Manager', 'Designer', 'Artist']
+// let arrSpecialization = team1.map(el => el.specialization) //['Manager', 'Designer', 'Designer', 'Artist']
+// let values = Object.values(salaries1)// [{…}, {…}, {…}]
+// for (let key in values) {
+//     valuesKey = values[key]
+//     console.log(valuesKey)
+// }
+// const getProductPrice = valuesKey => {
+   
+//      let salaryWithTax =  valuesKey.map(item => (item.salary / ((100 - parseInt(item.tax)) / 100)))
+// 	        return  console.log(Math.round(salaryWithTax))
+//  }
+
+  
+
+
+
+//  for (let i = 0; i < arrSpecialization.length; i++){
+//     let arrCurrent = arrSpecialization[i]
+//     if (keys.includes(arrCurrent)) {
+// 	    getProductPrice(valuesKey)
+// 		}
+// }
+
+
+
+// function calculateTeamFinanceReport(salaries, team) {
+
+// if (arrCurrent.includs(keys)) {
+// 		// let percent = (product.salePercent*100)/product.price;
+// 		// 		productPrice -= percent;
+//         console.log('hi')
+// 		}
+// }
+
+// const financeReport1 = calculateTeamFinanceReport(salaries1, team1)
+// console.log(JSON.stringify(financeReport1))
+
+
+
+
+const salaries1 = {
+Manager: { salary: 1000, tax: "10%" },
+Designer: { salary: 600, tax: "30%" },
+Artist: { salary: 1500, tax: "15%" },
+}
+const team1 = [
+{ name: "Misha", specialization: "Manager" },
+{ name: "Max", specialization: "Designer" },
+{ name: "Vova", specialization: "Designer"},
+{ name: "Leo", specialization: "Artist" },]
+let valuesKey;
+let keys = Object.keys(salaries1) //['Manager', 'Designer', 'Artist']
+let arrSpecialization = team1.map(el => el.specialization) //['Manager', 'Designer', 'Designer', 'Artist']
+let values = Object.values(salaries1)// [{…}, {…}, {…}]
+for (let key in values) {
+    valuesKey = values[key]
+   // console.log(valuesKey)
+}
+const getProductPrice = valuesKey => {
+   console.log(valuesKey)
+    let salaryWithTax = (valuesKey.salary / ((100 - parseInt(valuesKey.tax)) / 100))
+	        return  console.log(Math.round(salaryWithTax))
+ }
+
+  
+
+
+
+ for (let i = 0; i < arrSpecialization.length; i++){
+     let arrCurrent = arrSpecialization[i]
+    if (keys === arrCurrent) {
+        getProductPrice(valuesKey)
+    console.log(valuesKey)
+		}
+}
+
+
+
+
+
+// const financeReport1 = calculateTeamFinanceReport(salaries1, team1)
+// console.log(JSON.stringify(financeReport1))
